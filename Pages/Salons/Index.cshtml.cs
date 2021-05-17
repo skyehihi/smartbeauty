bb4707cd5c79c0c5529d1f29b12c788e2c4c33e9
Skye Nguyen <skyenguyen1193@gmail.com>
2021-05-17 05:14:01 -0700

    end lab 9

--- Pages/Salons/Index.cshtml.cs
+++ Pages/Salons/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SmartBeauty.Data;
 using SmartBeauty.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SmartBeauty.Pages.Salons
 {
@@ -19,11 +20,53 @@ namespace SmartBeauty.Pages.Salons
             _context = context;
         }
 
-        public IList<Salon> Salon { get;set; }
+        public PaginatedList<SmartBeauty.Models.Salon> Salon { get; set; }
+        public string SalonNameSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList SalonName { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SalonSalonName { get; set; }
+
+        public async Task OnGetAsync(string currentFilter, string sortOrder, string searchString, int? pageIndex)
+
         {
-            Salon = await _context.Salon.ToListAsync();
+            SalonNameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+
+            // Use LINQ to get list of genres.
+            IQueryable<SmartBeauty.Models.Salon> salonIQ = from s in _context.Salon
+                                                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                salonIQ = salonIQ.Where(s => s.SalonName.Contains(searchString)
+                                     );
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    salonIQ = salonIQ.OrderByDescending(s => s.SalonName);
+                    break;
+            }
+
+            int pageSize = 3;
+            Salon = await PaginatedList<SmartBeauty.Models.Salon>.CreateAsync(
+                salonIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
+
 }
