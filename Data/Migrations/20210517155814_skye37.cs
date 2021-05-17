ab15d602c51949d719eb7e76f9365ca36d856767
Skye Nguyen <skyenguyen1193@gmail.com>
2021-05-17 08:58:36 -0700

    new

---
+++ Data/Migrations/20210517155814_skye37.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartBeauty.Data.Migrations
+{
+    public partial class skye37 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityID",
+                table: "Salon");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityID",
+                table: "Salon",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
