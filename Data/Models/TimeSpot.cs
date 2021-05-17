7cdc54a147557d88a2a4a605a6698b0542d65bfe
Skye Nguyen <skyenguyen1193@gmail.com>
2021-05-17 03:09:31 -0700

    update data model

---
+++ Data/Models/TimeSpot.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartBeauty.Models
+{
+    public class TimeSpot
+    {
+        [Key]
+        public int TimeSpotID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string TimeSpotName { get; set; }
+
+        //public ICollection<Appointment> Appointments { get; set; }
+    }
+}
